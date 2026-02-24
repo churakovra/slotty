80cab79a7762c13706acf89a352b5d08ccac8c65
churakovra <churakov.main@gmail.com>
2026-02-24 23:46:50 +0300

    refactor: lint

--- app/notifier/service.py
+++ app/notifier/service.py
@@ -1,15 +1,14 @@
 class Notifier:
-    
     async def send_message(self, message: MessageTemplate):
-        '''
+        """
         should send income message. smth like
         Bot.send_message(**message)
         where message: MessageTemplate and contains key value pairs
         text=message_text: str
         reply_markup=markup: Optional[InlineMarkup/other markup]
         and other optional fields
-        '''
-        
+        """
+
         pass
-    
+
     pass
